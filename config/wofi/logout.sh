b9c9ec8b649b4a0975c13a4304e90866acfc2c43
Dmytro Ryzhykov <dark81@gmail.com>
2023-11-18 12:30:53 -0500

    Add wofi based menu for logout

---
+++ config/wofi/logout.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+WOFI_CMD="${WOFI_CMD:-wofi --dmenu --conf /home/dark81/.config/wofi/config_logout}"
+
+LOCK_SCREEN="󰌾 Lock Screen"
+LOGOUT="󰍃 Logout     "
+SHUTDOWN="󰐥 Power Off  "
+REBOOT="󰁪 Reboot     "
+
+chosen=$(echo -e "${LOCK_SCREEN}\n${LOGOUT}\n${SHUTDOWN}\n${REBOOT}\n" | $WOFI_CMD)
+
+if [[ $chosen = "${LOGOUT}" ]]; then
+	swaymsg exit
+elif [[ $chosen = "${SHUTDOWN}" ]]; then
+	systemctl poweroff
+elif [[ $chosen = "${REBOOT}" ]]; then
+	systemctl reboot
+elif [[ $chosen = "${LOCK_SCREEN}" ]]; then
+	swaylock -f -i ~/.config/wallpapers/wall_secondary.png
+fi
